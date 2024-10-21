16d24f237f4cac9fb7af87a9c91e29f40793c93a
Mark Tsai <marktw75@gmail.com>
2024-10-21 21:16:51 +0800

    初始化 Sprint Boot project

---
+++ src/main/java/com/marktsai/sprintbootmall/SprintbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.marktsai.sprintbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootMallApplication.class, args);
+    }
+
+}
