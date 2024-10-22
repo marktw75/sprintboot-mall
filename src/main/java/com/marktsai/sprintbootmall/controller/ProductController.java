c7b60a122ad776a19adba4af214a7539fe10768c
Mark Tsai <marktw75@gmail.com>
2024-10-22 19:36:30 +0800

    商品功能 - implement update product

--- src/main/java/com/marktsai/sprintbootmall/controller/ProductController.java
+++ src/main/java/com/marktsai/sprintbootmall/controller/ProductController.java
@@ -41,4 +41,22 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest) {
+        // check product exist or not
+        Product product = productService.getProductById(productId);
+
+        if(product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        // update product data
+        productService.updateProduct(productId, productRequest);
+
+        Product updatedProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
 }
