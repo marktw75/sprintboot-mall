ab7eaba9ac09c35bbac2ca2834a595c30e9eb7a3
Mark Tsai <marktw75@gmail.com>
2024-10-23 20:25:40 +0800

    商品功能 - implement delete product

--- src/main/java/com/marktsai/sprintbootmall/controller/ProductController.java
+++ src/main/java/com/marktsai/sprintbootmall/controller/ProductController.java
@@ -59,4 +59,11 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProductById(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
