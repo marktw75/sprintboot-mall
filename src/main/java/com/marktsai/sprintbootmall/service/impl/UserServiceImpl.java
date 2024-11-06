6c233f8dc5f51c27e20dd3c6384ed630535e5a27
Mark Tsai <marktw75@gmail.com>
2024-11-06 15:04:21 +0800

    帳號功能 - use MD5

--- src/main/java/com/marktsai/sprintbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/marktsai/sprintbootmall/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -36,6 +37,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // use MD5 to generate hash code
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // create account
         return userDao.createUser(userRegisterRequest);
     }
@@ -44,12 +49,17 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // check user's email is exist or not
         if (user == null) {
             log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        // use MD5 to generate password hash code
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // check password
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
